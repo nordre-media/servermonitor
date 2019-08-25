798ac74ff915c356e96a14e6c5901934c57424dc
Rezruel Leurzer <36955346+SaltMannen@users.noreply.github.com>
2019-08-25 23:17:29 +0200

    [Cleanup] remove unnecessary boolean check Check always results in true thanks to API stabilization.

--- src/main/kotlin/party/rezruel/servermonitor/listeners/LoginListener.kt
+++ src/main/kotlin/party/rezruel/servermonitor/listeners/LoginListener.kt
@@ -1,6 +1,5 @@
 package party.rezruel.servermonitor.listeners
 
-import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -12,7 +11,7 @@ class LoginListener(private val plugin: Monitor) : Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     fun onPlayerJoin(event: PlayerJoinEvent) {
-        if (event.player.isOnline and (event.player is Player)) {
+        if (event.player.isOnline) {
             event.player.sendMessage("Hjertelig...Velkommen...Til...Radio...Nordre...MEDIA!!!\n" +
                     "Husk å bli medlem i vår Discord: ${plugin.getLiveDiscordInvite()?.url}"
             )
